b56d9707e8a00c3ddbd1c79aba587ff4d0172df2
nick <nick@plusqa.com>
2020-09-03 17:38:55 -0700

    saving work

--- WiFiClient/WiFiClient.ino
+++ WiFiClient/WiFiClient.ino
@@ -11,13 +11,15 @@
 #include <iostream>
 #include "secrets.h"
 #include "helper.hpp"
-#include "ipscanner.h"
+#include "system_utils.hpp"
 
 const char* ssid     = STASSID;
 const char* password = STAPSK;
 
 const char* host = "192.168.1.205";
-const uint16_t PORT = 8080;
+const uint16_t PORT = 8000;
+
+#define DEBUG
 
 void setup() {
   Serial.begin(115200);
@@ -42,158 +44,95 @@ void setup() {
 
   Serial.println("");
   Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  // Setup blinking LED
+  pinMode(LED_BUILTIN, OUTPUT);
+  //led_blink(1, 250);
 }
 
-bool try_ip(const char* host, int port=PORT) {
-  Serial.print("connecting to ");
-  Serial.print(host);
-  Serial.print(':');
-  Serial.println(port);
 
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  if (!client.connect(host, port)) {
-    Serial.println("connection failed");
-    delay(500);
-    return false;
-  }
-  return true;
+void print_wifi_status() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
 }
 
-std::string make_ip_address(const char *ip_address) {
-  Serial.println((std::string("Incrementing ip address: ") + ip_address).c_str());
-  std::string ip_add(ip_address);
-  int last_period = ip_add.rfind('.');
-  std::string first_ip_section(ip_add.substr(0, last_period));
-  std::string last_ip_section(ip_add.substr(last_period));
-  int last_ip_section_int = strtol(last_ip_section.c_str(), NULL, 10);
-  ++last_ip_section_int;
-
-  std::string new_ip = first_ip_section + "." + helper::long_to_string(last_ip_section_int);
-  Serial.println(("New ip: " + new_ip).c_str());
-  return new_ip;
-}
-
-bool try_ip_address(const char *host) {
-  #ifdef DEBUG
-  Serial.print("connecting to ");
-  Serial.print(host);
-  Serial.print(':');
-  Serial.println(PORT);
-  #endif
-
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
+/*
+ * Test whether the current ip address is the base ip address.
+ */
+bool is_base_ip_address(WiFiClient &client, const char *host) {
   if (!client.connect(host, PORT)) {
     #ifdef DEBUG
     Serial.print("connection to ");
     Serial.print(host);
+    Serial.print(":");
+    Serial.print(PORT);
     Serial.println(" failed.");
-    delay(5000);
     #endif
+    client.stop();
     return false;
   }
   return true;
 }
 
-void f() {
-  /*
-  // This will send a string to the server
-  Serial.println("sending data to server");
-  if (client.connected()) {
-    //client.println("hello from ESP8266");
-    client.print(String("GET ") + "/" + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "User-Agent: BuildFailureDetectorESP8266\r\n" +
-               "Connection: close\r\n\r\n");
-  }
-
-  // wait for data to be available
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      delay(60000);
-      return false;
-    }
-  }
-  Serial.print("connection to ");
-  Serial.print(host);
-  Serial.println(" suceeded.");
-  return true;
-  */
-}
-
-std::string try_ip_addresses(const char *starting_ip, int starting_ip_end) {
+/*
+ * Find the base ip address from a starting ip address.
+ */
+const char* find_base_ip_address(WiFiClient &client, const char *starting_ip, int starting_ip_end) {
   std::string starting_ip_str(starting_ip);
   for (int i=starting_ip_end; i<starting_ip_end + 100; ++i) {
     std::string current_ip = (starting_ip) + helper::long_to_string(i);
-    bool success = try_ip_address(current_ip.c_str());
+    bool success = is_base_ip_address(client, current_ip.c_str());
     if (success) {
       Serial.print("Valid ip address: ");
       Serial.println(current_ip.c_str());
-      return current_ip;
+      return current_ip.c_str();
     }
   }
-  return std::string();
+  return NULL;
+}
+
+bool request_get(WiFiClient &client, const char* path)
+{
+  #ifdef DEBUG
+  Serial.print("Sending request to: \"");
+  Serial.print(path);
+  Serial.println("\"");
+  #endif // DEBUG
+  std::string full_request = std::string("GET ") + std::string(path) + std::string(" HTTP/1.1");
+  client.println(full_request.c_str());
+  client.println("Connection: close");
+  client.println();
+  delay(500);
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
+  Serial.println("Finished");
+  return false;
 }
 
 void loop() {
-  /*
-  Serial.print("connecting to ");
-  Serial.print(host);
-  Serial.print(':');
-  Serial.println(PORT);
-
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  if (!client.connect(host, PORT)) {
-    Serial.println("connection failed");
-    delay(5000);
-    return;
-  }
-
-  // This will send a string to the server
-  Serial.println("sending data to server");
-  if (client.connected()) {
-    //client.println("hello from ESP8266");
-    client.print(String("GET ") + "/" + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "User-Agent: BuildFailureDetectorESP8266\r\n" +
-               "Connection: close\r\n\r\n");
-  }
-
-  // wait for data to be available
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      delay(60000);
-      return;
-    }
-  }
-  */
-
-  /*
-  // Read all the lines of the reply from server and print them to Serial
-  Serial.println("receiving from remote server");
-  // not testing 'client.connected()' since we do not need to send data here
-  while (client.available()) {
-    char ch = static_cast<char>(client.read());
-    Serial.print(ch);
-  }
-
-  // Close the connection
-  Serial.println();
-  Serial.println("closing connection");
-  client.stop();
-  */
   while(true) {
-    try_ip_addresses("192.168.1.", 0);
-    delay(3000); // execute once every 5 minutes, don't flood remote service
+    WiFiClient client;
+    find_base_ip_address(client, "192.168.1.",197);
+    led_on();
+    print_wifi_status();
+    request_get(client, "/nick");
+    led_off();
+    delay(10000);
   }
 }
